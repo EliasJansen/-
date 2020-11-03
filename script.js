2ba9f4af79e827ee43b6eddc26f61e303c57e1c4
Elias Jansen <elias_jansen@hotmail.com>
2020-11-03 10:56:33 +0100

    update 3 november
    uodate 3 november

--- script.js
+++ script.js
@@ -98,3 +98,26 @@ let menuKnop = document.querySelector(".menu");
 			 	navMenu.classList.remove("onzichtbaar");
 			 }
 			 menuKnop.addEventListener("click",toonMenu);
+
+
+       // tekst
+
+       let atScroll = false;
+      let parallaxTitle = document.querySelectorAll(".parallax-title");
+
+      const scrollProgress = () => {
+          atScroll = true;
+      };
+
+      const raf = () => {
+          if (atScroll) {
+              parallaxTitle.forEach((element, index) => {
+                  element.style.transform = "translateX(" + window.scrollY / 8 + "%)";
+              });
+              atScroll = false;
+          }
+          requestAnimationFrame(raf);
+      };
+
+      requestAnimationFrame(raf);
+      window.addEventListener("scroll", scrollProgress);
